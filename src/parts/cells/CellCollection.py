b5a3a6d56fe2e3ffa0e9244eab3f98bbf94876fd
Terry Weymouth <weymouth@umich.edu>
2021-12-12 21:21:51 -0500

    Added one more Sensor - to support testing of making brain-network from genome.

--- src/parts/cells/CellCollection.py
+++ src/parts/cells/CellCollection.py
@@ -2,6 +2,7 @@ from settings import NUMBER_OF_NEURONS
 # Sensors
 from parts.cells.LocationEastWest import LocationEastWest
 from parts.cells.LocationNorthSouth import LocationNorthSouth
+from parts.cells.Age import Age
 # Actuators
 from parts.cells.MoveEastWest import MoveEastWest
 from parts.cells.MoveNorthSouth import MoveNorthSouth
@@ -12,7 +13,7 @@ from parts.cells.Neuron import Neuron
 class CellCollection:
 
     def __init__(self):
-        self.sensors = [LocationEastWest(), LocationNorthSouth()]
+        self.sensors = [LocationEastWest(), LocationNorthSouth(), Age()]
         self.actuators = [MoveEastWest(), MoveNorthSouth()]
         self.neurons = []
         while len(self.neurons) < NUMBER_OF_NEURONS:
